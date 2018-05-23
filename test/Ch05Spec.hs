e88bc6f73b04a34a5c0ffae2b7e190ae21f5c383
Stefano Tondo <stondo@gmail.com>
2018-05-24 01:00:46 +0200

    added missing test for Ch05 functions

--- test/Ch05Spec.hs
+++ test/Ch05Spec.hs
@@ -11,13 +11,28 @@ spec =
 
     describe "concat'" $ do
       it "concatenates a list of lists" $ do
-        show (concat' [[1,2,3],[99,88,45],[-1,-4,-9]]) `shouldBe` "[1,2,3,99,88,45,-1,-4,-9]"
+        show (concat' [[1, 2, 3], [99, 88, 45], [-1, -4, -9]]) `shouldBe` "[1,2,3,99,88,45,-1,-4,-9]"
 
+    describe "firsts" $ do
+      it "selects the first elements of each tuple in the list" $ do
+        show (firsts [(1, 2), (99, 88), (-1, -4)]) `shouldBe` "[1,99,-1]"
 
--- firsts
--- length'
--- factors
--- prime
--- primes
--- find
+    describe "length'" $ do
+      it "returns the length of the list" $ do
+        show (length' [1,2,3,99,88,45]) `shouldBe` "6"
 
+    describe "factors'" $ do
+      it "returns the factors of the given number" $ do
+        show (factors 15) `shouldBe` "[1,3,5,15]"
+
+    describe "prime" $ do
+      it "check if a number is prime" $ do
+        show (prime 7) `shouldBe` "True"
+
+    describe "primes" $ do
+      it "generates all prime number up to the given one" $ do
+        show (primes 10) `shouldBe` "[2,3,5,7]"
+
+    describe "find" $ do
+      it "returns a list of all values associated with a given key in a table" $ do
+        show (find 'a' [('a', 2),('c', 88),('a', -4)]) `shouldBe` "[2,-4]"
