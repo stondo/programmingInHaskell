4b451a61859179434dfad73dd1c3e96acc9471f4
Stefano Tondo <stondo@gmail.com>
2018-06-09 11:52:19 +0200

    Ch07 continued part 1

--- src/Ch07.hs
+++ src/Ch07.hs
@@ -1,6 +1,20 @@
 module Ch07
   ( twice
+  , sumsqreven
+  , sumsqreven'
+  , sumsqreven''
   ) where
 
 twice :: (a -> a) -> a -> a
 twice f x = f (f x)
+
+sumsqreven :: [Int] -> Int
+sumsqreven [] = 0
+sumsqreven (x:xs) | even x    = x^2 + sumsqreven xs
+                  | otherwise = sumsqreven xs
+
+sumsqreven' :: [Int] -> Int
+sumsqreven' xs = sum (map (^2) (filter even xs))
+
+sumsqreven'' :: [Int] -> Int
+sumsqreven'' = sum . map (^2) . filter even
--- test/Ch07Spec.hs
+++ test/Ch07Spec.hs
@@ -13,6 +13,19 @@ spec =
       it "applies a function twice to the given argument" $ do
         show (twice (*2) 3) `shouldBe` "12"
 
+    describe "sumsqreven" $ do
+      it "calculates the sum of the squares of a list of integers (recursion)" $ do
+        show (sumsqreven [1..10]) `shouldBe` "220"
+
+    describe "sumsqreven'" $ do
+      it "calculates the sum of the squares of a list of integers (map with explicit param)" $ do
+        show (sumsqreven' [1..10]) `shouldBe` "220"
+
+    describe "sumsqreven''" $ do
+      it "calculates the sum of the squares of a list of integers (map with composition and implicit param)" $ do
+        show (sumsqreven'' [1..10]) `shouldBe` "220"
+
+
 --    describe "" $ do
 --      it "" $ do
 --        show () `shouldBe` ""
