1082c9ba130beafdaeed1e86f75c8b2d24a01cdd
Stefano Tondo <stondo@gmail.com>
2018-06-09 19:40:30 +0200

    Ch07 continued part 2

--- src/Ch07.hs
+++ src/Ch07.hs
@@ -3,7 +3,24 @@ module Ch07
   , sumsqreven
   , sumsqreven'
   , sumsqreven''
+  , bin2int
+  , bin2int'
+  , int2bin
+  , make8
+  , encode
+  , chop8
+  , decode
+  , transmit
+  , channel
+  , votes
+  , count
+  , rmdups
+  , result
+  , winner
   ) where
+
+import Data.Char
+import Data.List
 
 twice :: (a -> a) -> a -> a
 twice f x = f (f x)
@@ -18,3 +35,64 @@ sumsqreven' xs = sum (map (^2) (filter even xs))
 
 sumsqreven'' :: [Int] -> Int
 sumsqreven'' = sum . map (^2) . filter even
+
+
+-- Binary String Transmitter
+type Bit = Int
+
+bin2int' :: [Bit] -> Int
+bin2int' bits = sum [w*b | (w,b) <- zip weights bits]
+               where weights = iterate (*2) 1
+
+bin2int :: [Bit] -> Int
+bin2int = foldr (\x y -> x + 2*y) 0
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+encode :: String -> [Bit]
+encode = concat . map (make8 . int2bin . ord)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 [] = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int) . chop8
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+channel :: [Bit] -> [Bit]
+channel = id
+
+-- Voting Algorithms
+
+-- First-past-the-post
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups []     = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+result :: Ord a => [a] -> [(Int,a)]
+result vs = sort [(count v vs, v) | v <- rmdups vs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+-- Alternative vote
+
+
+-- Exercises
+
+
+
--- test/Ch07Spec.hs
+++ test/Ch07Spec.hs
@@ -25,6 +25,53 @@ spec =
       it "calculates the sum of the squares of a list of integers (map with composition and implicit param)" $ do
         show (sumsqreven'' [1..10]) `shouldBe` "220"
 
+    describe "bin2int and bin2int' " $ do
+      it "convert a binary number in the form of an array and in reverse order to the corresponding integer" $ do
+        show (bin2int [1,0,1,1] == bin2int' [1,0,1,1]) `shouldBe` "True"
+
+    describe "int2bin" $ do
+      it "converts and integer to its corresponding binary number (output to an array with inverted order)" $ do
+        show (int2bin 13) `shouldBe` "[1,0,1,1]"
+
+    describe "make8" $ do
+      it "truncates or extends a binary number to make it precisely 8 bits" $ do
+        show (make8 [1,0,1,1]) `shouldBe` "[1,0,1,1,0,0,0,0]"
+
+    describe "encode" $ do
+      it "encodes a string of characters in a list of concatenated 8 bits binary number corresponding to the Unicode number of each character" $ do
+        show (encode "abc") `shouldBe` "[1,0,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0]"
+
+    describe "chop8" $ do
+      it "chops a list of bits into 8 bit binary number" $ do
+        show (chop8 [1,0,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0]) `shouldBe` "[[1,0,0,0,0,1,1,0],[0,1,0,0,0,1,1,0],[1,1,0,0,0,1,1,0]]"
+
+    describe "decode" $ do
+      it "decodes a list of bits as a string" $ do
+        show (decode [1,0,0,0,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0]) `shouldBe` "\"abc\""
+
+    describe "transmit" $ do
+      it "simulates the transmisison of a string of characters as a list of bits using a perfect communication channel" $ do
+        show (transmit "antani sbiricuda cum sblindosterno a destra!") `shouldBe` "\"antani sbiricuda cum sblindosterno a destra!\""
+
+    describe "channel" $ do
+      it "Bit type identity function" $ do
+        show (channel [1,0,1,1,0,0,0,0]) `shouldBe` "[1,0,1,1,0,0,0,0]"
+
+    describe "count" $ do
+      it "counts the element in a list that are equals to the given one" $ do
+        show (count "Red" votes) `shouldBe` "2"
+
+    describe "rmdups" $ do
+      it "removes duplicates from a list" $ do
+        rmdups votes `shouldBe` ["Red", "Blue", "Green"]
+
+    describe "result " $ do
+      it "returns the result of a first-past-the-post election in increasing order of the number of votes received" $ do
+        result votes `shouldBe` [(1,"Green"),(2,"Red"),(3,"Blue")]
+
+    describe "winner" $ do
+      it "is the last element in the result list" $ do
+        winner votes `shouldBe` "Blue"
 
 --    describe "" $ do
 --      it "" $ do
