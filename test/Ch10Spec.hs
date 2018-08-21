d014bd49df5776473339ff0af9ea24343497181c
Stefano Tondo <stondo@gmail.com>
2018-08-21 02:49:25 +0200

    added dummy test to Ch10Spec

--- test/Ch10Spec.hs
+++ test/Ch10Spec.hs
@@ -1,2 +1,19 @@
 module Ch10Spec where
 
+import Ch10
+
+import Test.Hspec (Spec, describe, context, it, shouldBe)
+
+
+-- |Required for auto-discpvery
+spec :: Spec
+spec =
+  describe "Ch10 functions" $ do
+
+--    describe "" $ do
+--      it "" $ do
+--        () `shouldBe`
+
+   describe "dummy test" $ do
+     it "should pass" $ do
+        "" `shouldBe` ""
