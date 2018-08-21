2f11232dfde63661228b8650dd92bcb58ab3d61f
Stefano Tondo <stondo@gmail.com>
2018-08-21 12:14:27 +0200

    tictactoe human vs human completed

--- src/Ch11.hs
+++ src/Ch11.hs
@@ -5,12 +5,31 @@ module Ch11
   , next
   , empty
   , full
+  , turn
+  , wins
+  , diag
+  , won
+  , putGrid
+  , showRow
+  , showPlayer
+  , interleave
+  , valid
+  , move
+  , chop
+  , getNat
+  , tictactoe
+  , run
+  , run'
+  , prompt
   ) where
 
 import Data.Char
 import Data.List
 import System.IO
 
+import Ch10 (cls, goto)
+
+-- 11.2 Basic declarations
 size :: Int
 size = 3
 
@@ -23,8 +42,94 @@ next O = X
 next B = B
 next X = O
 
+-- 11.3 Grid utilities
 empty :: Grid
 empty = replicate size (replicate size B)
 
 full :: Grid -> Bool
 full = all (/= B) . concat
+
+turn :: Grid -> Player
+turn g = if os <= xs then O else X
+         where os = length (filter (== O) ps)
+               xs = length (filter (== X) ps)
+               ps = concat g
+
+wins :: Player -> Grid -> Bool
+wins p g = any line (rows ++ cols ++ dias)
+           where
+             line = all (== p)
+             rows = g
+             cols = transpose g
+             dias = [diag g, diag (map reverse g)]
+
+diag :: Grid -> [Player]
+diag g = [g !! n !! n | n <- [0..size-1]]
+
+won :: Grid -> Bool
+won g = wins O g || wins X g
+
+-- 11.4 Displaying a grid
+putGrid :: Grid -> IO ()
+putGrid = putStrLn . unlines . concat . interleave bar . map showRow
+          where bar = [replicate ((size*4)-1) '-']
+
+showRow :: [Player] -> [String]
+showRow = beside . interleave bar . map showPlayer
+  where beside = foldr1 (zipWith (++))
+        bar    = replicate 3 "|"
+
+showPlayer :: Player -> [String]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+showPlayer X = ["   ", " X ", "   "]
+
+interleave :: a -> [a] -> [a]
+interleave x []     = []
+interleave x [y]    = [y]
+interleave x (y:ys) = y : x : interleave x ys
+
+-- 11.5 Making a move
+valid :: Grid -> Int -> Bool
+valid g i = 0 <= i && i < size^2 && concat g !! i == B
+
+move :: Grid -> Int -> Player -> [Grid]
+move g i p = if valid g i then [chop size (xs ++ [p] ++ ys)] else []
+  where (xs,B:ys) = splitAt i (concat g)
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
+
+-- 11.6 Reading a number
+getNat :: String -> IO Int
+getNat prompt = do putStr prompt
+                   xs <- getLine
+                   if xs /= [] && all isDigit xs then
+                      return (read xs)
+                   else
+                      do putStrLn "ERROR: invalid number"
+                         getNat prompt
+
+-- 11.7 Human vs human
+tictactoe :: IO ()
+tictactoe = run empty O
+
+run :: Grid -> Player -> IO ()
+run g p = do cls
+             goto (1,1)
+             putGrid g
+             run' g p
+
+run' :: Grid -> Player -> IO ()
+run' g p | wins O g = putStrLn "Player O wins!\n"
+         | wins X g = putStrLn "Player X wins!\n"
+         | full g   = putStrLn "It's a draw!\n"
+         | otherwise = do i <- getNat (prompt p)
+                          case move g i p of
+                             [] -> do putStrLn "ERROR: Invalid move"
+                                      run' g p
+                             [g'] -> run g' (next p)
+
+prompt :: Player -> String
+prompt p = "Player " ++ show p ++ ", enter your move: "
